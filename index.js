4173d51ed2d10e5ad80331ff0a01c6405c398634
Alexey Nikitenko <nikitenko_1504@mail.ru>
2023-12-04 21:18:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import "./index.css";
+import moment from "moment";
+
+const form = document.querySelector(".donate-form");
+const input = document.querySelector(".donate-form__donate-input");
+
+const donateList = document.querySelector(".donates-container__donates");
+const totalDonateValue = document.querySelector("#total-amount");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  totalDonateValue.innerText = `${getNumberOfDollars() + Number(input.value)}$`;
+  donateList.innerHTML += DonateInfo(input.value);
+});
+
+function DonateInfo(donate) {
+  const donateDate = new Date(Date.now());
+  const date = moment(donateDate).format("MMMM Do YYYY, h:mm:ss a");
+  return `<div class="donate-item">${date} - <b>${donate}$</b></div>`;
+}
+
+function getNumberOfDollars() {
+  const totalDollars = totalDonateValue.innerText;
+  return Number(totalDollars.substring(0, totalDollars.length - 1));
+}
